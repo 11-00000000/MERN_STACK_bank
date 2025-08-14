4fd53eb6c799599e1c1e479498cad9887a1a3da1
samata-saha <samatasaha03@gmail.com>
2025-08-14 11:04:42 +0530

    .

--- frontend/src/components/reusable/CustomAuthButton.jsx
+++ frontend/src/components/reusable/CustomAuthButton.jsx
@@ -12,8 +12,8 @@ const CustomAuthButton = ({
     <>
             <button  type={type} {...props} disabled={isLoading} className={clsx(className,
 
-isLoading&&"disabled:bg-rose-700 " ,
-"w-full flex items-center justify-center capitalize text-white text-lg bg-rose-600 py-3 gap-x-2 rounded shadow"
+isLoading&&"disabled:bg-rose-700 text-white" ,
+"w-full flex items-center justify-center capitalize text-lg bg-rose-600 py-3 gap-x-2 rounded shadow"
 
             )}> <span>{text}</span> { isLoading&&<CgSpinner  className='animate-spin text-xl' />} </button>
     </>
