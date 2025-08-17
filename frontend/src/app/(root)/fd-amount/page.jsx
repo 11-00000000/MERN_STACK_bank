37ba7e506f660ba4ca40a470d73163f64f09d334
Saikat Mitra <mitrasaikat265@gmail.com>
2025-08-17 20:31:27 +0530

    error fd

--- frontend/src/app/(root)/fd-amount/page.jsx
+++ frontend/src/app/(root)/fd-amount/page.jsx
@@ -12,13 +12,16 @@ const FDpage = () => {
      <HeaderName/>
      <div className="py-10 grid grid-cols-1 items-start gap-x-4 gap-y-3 lg:grid-cols-2 xl:grid-cols-3">
    
-     <AddNewFdModel/> {
-      Array(4).fill(null).map((cur,i)=>{
-        return<FDCard key={i}/>
-      })
-    }
-    
-    
+     <AddNewFdModel/> 
+     {
+  Array(4).fill(null).map((cur, i) => {
+    const fdData = { amount: 10000 + i * 5000 }; // Example data
+    return <FDCard key={i} fd={fdData} />;
+  })
+}
+
+
+
     
    </div>
       </div>
