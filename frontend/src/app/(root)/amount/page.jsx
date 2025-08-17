3a46a3793cafdc6d769dbd21b6a77db10e262492
Sampriti26 <sadhukhansampriti@gmail.com>
2025-08-17 17:50:04 +0530

    fd

--- frontend/src/app/(root)/amount/page.jsx
+++ frontend/src/app/(root)/amount/page.jsx
@@ -4,19 +4,23 @@ import React, { Suspense, useState } from "react";
 
 import AddAmountModel from "@/components/Amount/AddAmountModel";
 import { useMainContext } from "@/context/MainContext";
-// import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 // import AddAccountModal from './+__(components)/AddAccountModal';
 // import CustomLoader from '@/components/reuseable/CustomLoader';
 
 const AmountPage = () => {
+   const [isShow,setIsShow] = useState(false)
   const { user } = useMainContext();
 
   return (
     <>
       <div className="container py-10">
         <HeaderName />
+         <div className="grid  grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-2 gap-y-3">
+{
 
-        <div className="card w-full border py-5 rounded flex items-center justify-between px-3">
+
+ <div className="card w-full border py-5 rounded flex items-center justify-between px-3">
           <div className="flex flex-col">
             <h1 className="text-2xl font-bold">Add Amount</h1>
             <p className="text-lg text-zinc-500 font-medium">
@@ -25,11 +29,21 @@ const AmountPage = () => {
             <p className="text-3xl font-semibold">
               Total Amount : {user.amount}/-
             </p>
+
+
+
+
+            
           </div>
+
+
 
           <AddAmountModel id={user.account_no} />
         </div>
+    
+  }
       </div>
+    </div>
     </>
   );
 };
--- frontend/src/app/(root)/fd-amount/page.jsx
+++ frontend/src/app/(root)/fd-amount/page.jsx
@@ -1,67 +1,29 @@
-"use client";
+ "use client";
 import HeaderName from '@/components/HeaderName'
-import React, { Suspense, useEffect, useState } from 'react'
-import AddNewFdModel from './+___compoents/AddNewFdModel'
-import FDCard from './+___compoents/FDCard'
-import { axiosClient } from '@/utils/AxiosClient'
-import CustomLoader from '@/components/reuseable/CustomLoader'
-import { toast } from 'react-toastify';
+import React from 'react'
+import AddNewFdModel from './+___compoents/AddNewFDModel'
+import FDCard from './+___compoents/FDCard';
 
-const FDPage = () => {
-  const[deposits,setDeposists] = useState([])
-
-  const [loading,setLoading] = useState(true)
-  const [isUpdate,setIsUpdate] = useState(false)
-
-  const fetchAllDeposits = async()=>{
-    
-    try {
-      setLoading(true)
-      const response = await axiosClient.get('/fd/get-all',{
-        headers:{
-          'Authorization':'Bearer '+localStorage.getItem("token")
-        }
-      })
-
-      const data =await response.data 
-      setDeposists(data)
-
-    } catch (error) {
-      toast.error(error.data.response.msg || error.message)
-    }finally{
-      setLoading(false)
-    }
-  }
-  useEffect(()=>{
-    fetchAllDeposits()
-  },[isUpdate])
-  if(loading){
-    return <div className="w-full min-h-screen flex justify-center items-center">
-      <CustomLoader/>
-      </div>
-  }
-
+const FDpage = () => {
   return (
     <>
-            <div className="container py-10">
-            <HeaderName/>
 
-            <div className="py-10 grid grid-cols-1  items-start gap-x-4 gap-y-3 lg:grid-cols-2 xl:grid-cols-3">
-
-            <AddNewFdModel isUpdate={isUpdate} setIsUpdate={setIsUpdate} />   
-            <Suspense fallback={<CustomLoader/>}>
-            {
-                 deposits.length>0&&           deposits.map((cur,i)=>{
-                                return <FDCard  isUpdate={isUpdate} setIsUpdate={setIsUpdate} key={i} data={cur} />
-                            })
-                        }  
-            </Suspense>
-
-            </div>
-            </div>
+   <div>
+     <HeaderName/>
+     <div className="py-10 grid grid-cols-1 items-start gap-x-4 gap-y-3 lg:grid-cols-2 xl:grid-cols-3">
    
-   </>
+     <AddNewFdModel/> {
+      Array(4).fill(null).map((cur,i)=>{
+        return<FDCard key={i}/>
+      })
+    }
+    
+    
+    
+   </div>
+      </div>
+    </>
   )
 }
 
-export default FDPage
+export default FDpage
