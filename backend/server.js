051ad978ed27af728a62b3164a4719abca165806
Saikat Mitra <mitrasaikat265@gmail.com>
2025-08-25 15:25:46 +0530

    dhur
    hoyeni integrate

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,13 @@ config({
 const app = require('./src/app')
 const { ConnectDB } = require('./src/config/db.config')
 const port = process.env.PORT || 8000
+const chatRoutes = require('./src/router/chatRoutes');
 //const userRoutes = require("./src/router/userRoutes");
 //app.use("/api/user", userRoutes);
 
 ConnectDB()
+
+app.use("/api/chat", chatRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
---
+++ frontend/src/app/widget/app.jsx
@@ -0,0 +1,12 @@
+import Chatbot from "./Chatbot";
+
+function App() {
+  return (
+    <div>
+      {/* your banking dashboard */}
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
